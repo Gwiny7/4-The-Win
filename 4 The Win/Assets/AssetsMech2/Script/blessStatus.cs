b7ded6e609623bf5f448ce68a9c8b86f5021c7b8
Gwiny7 <guischwetz@gmail.com>
2022-10-24 20:04:11 -0300

    protótipo aleatório

--- 4 The Win/Assets/AssetsMech2/Script/blessStatus.cs
+++ 4 The Win/Assets/AssetsMech2/Script/blessStatus.cs
@@ -6,9 +6,14 @@ using Photon.Pun;
 public class blessStatus : MonoBehaviourPunCallbacks
 {
     public ProjectileSpawner LocalPlayer;
+    private int Seed;
+
     void Awake()
     {
-        if(PhotonNetwork.LocalPlayer.ActorNumber == PlayerArrayControl.PlayersActorOrder[0]){
+        //Seed = Random.Range(0, 64);
+        //Random.seed = (int)Seed;
+
+        if(PhotonNetwork.LocalPlayer.ActorNumber == PlayerArrayControl.PlayersActorOrder[/*(int)Random.Range(0, PhotonNetwork.CurrentRoom.PlayerCount)*/0]){
             LocalPlayer.SetBlessed(true);
         }
 
