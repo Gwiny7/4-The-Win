6e121e0c9f620cb454b77f8874e7a5449883f7c1
Pancatov <albertoddczolet@gmail.com>
2022-11-25 22:28:59 -0300

    LoadingScreens, sem build pq tava bugada

--- 4 The Win/Assets/AssetsMech4/PlayerStatus.cs
+++ 4 The Win/Assets/AssetsMech4/PlayerStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
@@ -13,9 +14,10 @@ public class PlayerStatus : MonoBehaviour
     private bool blessed;
     public int maxTry;
     public int tryLeft;
+    public List<GameObject> Lifes = new List<GameObject>();
+    public Sprite brokenHeart;
     public GameObject victoryScreen;
     public GameObject blessedStatus;
-    public TMP_Text tryStatus;
     
     void Awake(){
         PV = GetComponent<PhotonView>();
@@ -23,20 +25,27 @@ public class PlayerStatus : MonoBehaviour
     }
     void Start()
     {
+        for(int i=0; i<9-tryLeft;i++)
+        {
+            Destroy(Lifes[tryLeft]);
+            Lifes.Remove(Lifes[tryLeft]);
+            Debug.Log("foi");
+        }
         StartCoroutine(Wait(5.0f));
         
     }
 
     void Update(){
-        tryStatus.text = ("Tries Left: " + tryLeft);
+        
         if(blessed){
             blessedStatus.SetActive(true);
         }
+
     }
 
     public void Try()
     {
-        tryLeft--;
+        lostLife();
     }
 
     public bool GetBlessed(){
@@ -60,6 +69,15 @@ public class PlayerStatus : MonoBehaviour
 
     public void SetVictory(){
         PV.RPC("RPC_Victory", RpcTarget.AllBuffered);
+    }
+
+    public void lostLife()
+    {
+        Lifes[tryLeft-1].GetComponent<Image>().sprite = brokenHeart;
+        Lifes[tryLeft-1].GetComponent<Image>().color = Color.white;
+        Lifes.Remove(Lifes[Lifes.Count-1]);
+        tryLeft--;
+        Debug.Log("Lifes left: " + tryLeft);
     }
 
     [PunRPC]
--- 4 The Win/Assets/LoadingScreen.cs
+++ 4 The Win/Assets/LoadingScreen.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject button;
+    public float timeToShow;
     void Start()
     {
-        
+        StartCoroutine(ButtonSpawner());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator ButtonSpawner()
+    {
+        yield return new WaitForSeconds(timeToShow);
+
+        button.SetActive(true);
+
+
     }
 }
